c8b1430b409dce58983d971e5feb9ed969e82e8e
d3h1 <denicabaravdic@gmail.com>
2023-10-12 16:26:33 -0700

    testing some python

--- Python Notes/pythonTesting.py
+++ Python Notes/pythonTesting.py
@@ -1,5 +1,18 @@
-my_list = [1,2,3,4,5,6]
-start, end = 1, 4
+even_list = [1, 2, 3, 4, 5, 6]
 
-print(my_list[:start])
-print(my_list[end+1:])
+counter = int(len(even_list) / 2)
+
+new_list = even_list[counter-1:counter+1]
+done = new_list[0] + new_list[1]
+print(done / 2)
+
+odd_list = [1,2,3,4,5]
+x = len(odd_list) / 2
+print(x)
+
+
+
+
+
+
+
